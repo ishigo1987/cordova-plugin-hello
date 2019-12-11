ff8aab4ae4f728dab043750f9f025fb83671ca83
ishigo1987 <stephnguets@yahoo.fr>
2019-12-11 20:50:48 +0100

    Update

--- www/hello.js
+++ www/hello.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
-
+const exec = require('cordova/exec');
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+        exec(successCallback, errorCallback, "deleteSms", "delete", [name]);
     }
 };
