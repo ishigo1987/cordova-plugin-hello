51e91ff8cf951bf468cab2aad369b2fb6e3d6fa7
ishigo1987 <stephnguets@yahoo.fr>
2019-12-11 20:53:45 +0100

    Update

--- www/deleteSms.js
+++ www/deleteSms.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 const exec = require('cordova/exec');
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
+    delete: function (name, successCallback, errorCallback) {
         exec(successCallback, errorCallback, "deleteSms", "delete", [name]);
     }
 };
